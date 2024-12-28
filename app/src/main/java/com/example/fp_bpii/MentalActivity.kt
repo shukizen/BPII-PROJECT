51688e17653fc5cc14628ada53e634ac198f9874
Hatta PS <hattajunior1@gmail.com>
2024-12-28 21:43:13 +0700

    tips sama mental udah jalan

--- app/src/main/java/com/example/fp_bpii/MentalActivity.kt
+++ app/src/main/java/com/example/fp_bpii/MentalActivity.kt
@@ -2,38 +2,28 @@ package com.example.fp_bpii
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity2 : AppCompatActivity() {
+class MentalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home2)
+        setContentView(R.layout.activity_mental)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        val btnTips: LinearLayout = findViewById(R.id.Tips)
+        val btnBack : ImageButton = findViewById(R.id.btn_back)
 
-        btnTips.setOnClickListener {
-            val intent = Intent(this, TipsActivity::class.java)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, HomeActivity2::class.java)
             startActivity(intent)
-
-    }
-
-        val btnMental: LinearLayout = findViewById(R.id.Mental)
-
-        btnMental.setOnClickListener {
-            val intent = Intent(this, MentalActivity::class.java)
-            startActivity(intent)
-
         }
+
     }
 }
